e64168b270e0b9dc5ccc8c3b538a7896634f2afc
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 00:15:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,7 @@ def adding_matz
 # }
 
 	programmer_hash =
- 		{			  
+ 		{
 			  :yukihiro_matsumoto => {
 			    :known_for => "Ruby",
 				  :languages => ["LISP", "C"]
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  
+
 
 end
 
