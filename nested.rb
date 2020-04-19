1fd108ef1c21ecded6d58c124c7433ca47998650
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 00:21:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,9 +100,13 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+     },
+		 :yukihiro_matsumoto => {
+			 :known_for => "Ruby",
+			 :languages => ["LISP", "C"]
+		 }
+	}
+     programmer_hash[:alan_kay][:known_for] = "GUI"
 
 
 end
