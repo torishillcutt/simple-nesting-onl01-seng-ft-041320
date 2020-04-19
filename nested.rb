8cd5b32f5421383e4084205d3e3d563c9c5d51b9
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 00:25:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,8 +107,6 @@ def changing_alan
 	}
   programmer_hash[:alan_kay][:known_for] = "GUI"
 	programmer_hash
-
-
 end
 
 def adding_to_dennis
@@ -127,5 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]  << "Assembly"
+	programmer_hash 
 
 end
